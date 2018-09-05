bc7e4291968b56114be1d2c1e5ae3f34b55f8340
Nano Bacano <ricardo.bentin@gmail.com>
2018-09-05 15:45:02 -0400

    i have a working match logic!

--- server.js
+++ server.js
@@ -10,12 +10,24 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Friends list(DATA)
 // =============================================================
-let friends = [];
+let friends = [
+  {
+    name: "Ed",
+    photo: "https://data.whicdn.com/images/30847676/large.jpg",
+    ans: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: "Brad",
+    photo:
+      "https://static.highsnobiety.com/wp-content/uploads/2017/07/31124806/brad-pitt-workout-routine-2.jpg",
+    ans: [1, 5, 3, 2, 5, 4, 3, 2, 1, 1]
+  }
+];
 
 //route for catch all
 app.get("/", function(req, res) {
@@ -42,16 +54,48 @@ app.get("/api/friends", function(req, res) {
 });
 //route for adding new friends by doing a post to api friends
 app.post("/api/friends", function(req, res) {
+  //capture the user input object - passed as userData from HTML
   let newfriend = req.body;
   console.log("This is a new friend\n", newfriend);
-  friends.push(newfriend);
+  let newfriendAns = req.body.ans;
 
-  for (var i = 0; i < friends.length; i++) {
-    console.log(friends[i].ans);
+  //match logic
+  let matchName = "";
+  let matchImage = "";
+  let totalDifference = 10000; // Make the initial value big for comparison
+
+  //do the comparison for everyone in the friends arra
+  for (let i = 0; i < friends.length; i++) {
+    // Compute differences for each question
+    let diff = 0;
+    for (var j = 0; j < newfriendAns.length; j++) {
+      diff += Math.abs(friends[i].ans[j] - newfriendAns[j]);
+    }
+    console.log('diff = ' + diff);
+
+    // If lowest difference, record the friend match
+    if (diff < totalDifference) {
+      console.log('Closest match found = ' + diff);
+      console.log('Friend name = ' + friends[i].name);
+      console.log('Friend image = ' + friends[i].photo);
+
+      totalDifference = diff;
+      matchName = friends[i].name;
+      matchImage = friends[i].photo;
+
+      console.log("this is match name:", matchName)
+      console.log("this is match image:", matchImage)
+    }
   }
 
-  res.json(newfriend);
-  //logic for compatibility match
+  //add a new friend
+  friends.push(newfriend);
+
+  console.log("this is match name:", matchName)
+  console.log("this is match image:", matchImage)
+  //send the response back to the html page
+  res.json({matchName: matchName, matchImage: matchImage});
+  
 });
 
 // Starts the server to begin listening
